a275bf1c63e44ccc4f024edf91d568bd14f04702
Anas986 <60493038+DcWire@users.noreply.github.com>
2023-06-21 14:56:01 +0530

    Create basic server and folders

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+// const expressValidator = require('express-validator');
+const morgan = require('morgan');
+const path = require('path');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+// app.use(expressValidator());
+app.use(morgan('dev'));
+
+const PORT = process.env.PORT || 8005;
+
+app.listen(PORT, () => {
+    console.log("Server is running on port: " + PORT);
+})
+
