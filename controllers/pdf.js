84b1e4f4051087988c39fadc7bcc53b59a243d0d
Anas986 <60493038+DcWire@users.noreply.github.com>
2023-06-23 01:12:19 +0530

    Add controller that converts html string to pdf and also used to accept request from client

---
+++ controllers/pdf.js
@@ -0,0 +1,33 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+exports.generatePDF = async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    
+    const html = `
+        <html>
+            <head>
+                <title>PDF Test</title>
+            </head>
+            <body>
+                <h1>Hello World</h1>
+            </body>
+        </html>
+    `;
+
+    await page.setContent(html);
+    await page.pdf({
+        path: 'biodata.pdf',
+        format: 'A4',
+        margin: {
+        top: '10mm',
+        bottom: '10mm',
+        left: '10mm',
+        right: '10mm',
+        },
+    });
+
+
+    await browser.close();
+}
--- routes/pdf.js
+++ routes/pdf.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
+const { generatePDF } = require('../controllers/pdf');
 
 router.post('/create-pdf', (req, res) => {
     console.log(req.body);
+    generatePDF();
 })
 
 module.exports = router;
